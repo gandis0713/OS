7a1ba1d6fcc4cb2a46106b67e346253aa1bfb541
gandis <gandis0713@gmail.com>
2018-08-25 15:29:12 +0900

    Revert "Issue:1"
    This reverts commit 88ed7b0f4d54152e2fa2eac913d58685b0536044.

--- StartOS.sh
+++ StartOS.sh
@@ -5,4 +5,4 @@ make REBUILD
 OSPID="$(pgrep -a qemu)"
 kill -9 $OSPID
 
-qemu-system-x86_64 -L . -m 64 -s -cdrom out/OS.iso -localtime -M pc
+qemu-system-x86_64 -L . -m 64 -s -fda out/OS.img -localtime -M pc
